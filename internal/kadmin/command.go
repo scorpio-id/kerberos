4a1154d70a0412c158720eaee24cdea31d768fc3
jplegacy <jeremyperez1122@gmail.com>
2024-07-16 16:51:33 -0400

    Delete + Modify Principal

--- internal/kadmin/command.go
+++ internal/kadmin/command.go
@@ -9,25 +9,82 @@ import (
 )
 
 type Kadmin struct {
-	Password string
-	mu      sync.RWMutex
+	password 	string
+	cmd	 		*exec.Cmd
+	mu       	sync.RWMutex
+}
+
+func NewKadmin(password string) *Kadmin {
+	return &Kadmin{
+		password: "password",
+		cmd: &exec.Cmd{},
+	}
 }
 
 func (kadmin *Kadmin) CreatePrincipal(principal string, password string) error {
-	// set up command 
-	cmd := exec.Command("kadmin", "-w", "resetme", "add_principal", "-pw", password, principal)
-    var out bytes.Buffer
-    cmd.Stdout = &out
-
 	// lock & unlock kadmin
 	kadmin.mu.Lock()
 	defer kadmin.mu.Unlock()
 
+	// set up command 
+	kadmin.cmd = exec.Command("kadmin", "-w", "resetme", "add_principal", "-pw", password, principal)
+    var out bytes.Buffer
+    kadmin.cmd.Stdout = &out
+
 	// execute command
-    err := cmd.Run()
+    err := kadmin.cmd.Run()
 	if err != nil {
 		return err
 	}
+
+	// reset command buffer
+	kadmin.cmd = &exec.Cmd{}
+
+	// all good!
+	return nil
+}
+
+// TODO: Review ACLs - https://docs.oracle.com/cd/E36784_01/html/E37126/aadmin-14.html#scrolltoc
+func (kadmin *Kadmin) DeletePrincipal(principal string) error {
+	// lock & unlock kadmin
+	kadmin.mu.Lock()
+	defer kadmin.mu.Unlock()
+
+	// set up command 
+	kadmin.cmd = exec.Command("kadmin", "-w", "resetme", "delete_principal", "-force", principal)
+    var out bytes.Buffer
+    kadmin.cmd.Stdout = &out
+
+	// execute command
+    err := kadmin.cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	// reset command buffer
+	kadmin.cmd = &exec.Cmd{}
+
+	// all good!
+	return nil
+}
+
+func (kadmin *Kadmin) ChangePrincipalPassword(principal string, newPassword string) error {
+	// lock & unlock kadmin
+	kadmin.mu.Lock()
+	defer kadmin.mu.Unlock()
+
+	// set up command 
+	kadmin.cmd = exec.Command("kadmin", "-w", "resetme", "change_password", "-pw", newPassword, principal)
+    var out bytes.Buffer
+    kadmin.cmd.Stdout = &out
+
+	// execute command
+    err := kadmin.cmd.Run()
+	if err != nil {
+		return err
+	}
+	// reset command buffer
+	kadmin.cmd = &exec.Cmd{}
 
 	// all good!
 	return nil
@@ -35,9 +92,8 @@ func (kadmin *Kadmin) CreatePrincipal(principal string, password string) error {
 
 // web handler
 func PrincipalHandler(w http.ResponseWriter, r *http.Request) {
-	
 	kadmin := Kadmin {
-		Password: "resetme",
+		password: "resetme",
 	}
 
 	err := kadmin.CreatePrincipal("hello-from-golang", "resetme")
