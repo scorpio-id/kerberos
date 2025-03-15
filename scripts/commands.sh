55c2fcbaeb6ad7f6c8b09cbc030e4ca3c74719d5
brandon <b.mazey@nyu.edu>
2025-03-15 11:06:22 +0000

    starting krb db

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -2,8 +2,12 @@
 
 # add kerberos services
 openrc default
+
 rc-update add krb5kdc default
+rc-update add krb5kadmind default
+
 service krb5kdc start
+service krb5kadmind start
 
 # start application
 /scorpio-kerberos
