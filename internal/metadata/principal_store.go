ca2ecdbd2cb9fb64c947a552312e7c2ac75885dd
brandon <b.mazey@nyu.edu>
2025-09-16 17:19:11 -0400

    CRD operations on principal store

--- internal/metadata/principal_store.go
+++ internal/metadata/principal_store.go
@@ -15,4 +15,42 @@ type Account struct {
 	IsServicePrincipal bool   `json:"is_service_principal"`
 }
 
-// TODO basic CRUD operations
+// TODO document
+func NewStore() Store {
+	return Store {
+		AccessControlList: make(map[string]string),
+		Accounts: make([]Account, 0),
+	}
+}
+
+func(s *Store) Add(a Account) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.Accounts = append(s.Accounts, a)
+}
+
+func(s *Store) Delete(principal string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for i, v := range s.Accounts {
+		if v.PrincipalName == principal {
+			s.Accounts = append(s.Accounts[:i], s.Accounts[i+1:]...)
+			break
+		}
+	}
+}
+
+func(s *Store) Contains(principal string) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	for _, v := range(s.Accounts) {
+		if v.PrincipalName == principal {
+			return true
+		}
+	}
+
+	return false
+}
